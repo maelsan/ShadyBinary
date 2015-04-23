346bb2a5df4264e30bc3a5e8eb8cf11590fc3cf4
Yanis A <solikate@users.noreply.github.com>
2015-04-23 15:43:48 +0200

    Deleting struct

--- include/network.h
+++ include/network.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "struct.h"
 #include "const.h"
 
 extern int forceUri(char *uri);
--- include/struct.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHADY_STRUCT
-#define SHADY_STRUCT
-
-typedef struct {
-  char *uri;
-  struct shadyLogs *next;
-} shadyLogs;
-
-#endif
