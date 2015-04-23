13d3e80fc054d37d695dfbf04172ac9eebdce3f8
Yanis A <solikate@users.noreply.github.com>
2015-04-23 10:35:44 +0200

    Transmission of words.

--- src/network.c
+++ src/network.c
@@ -5,11 +5,15 @@ int forceUri(char *uri, shadyLogs *results)
 {
   CURL *request = curl_easy_init();
   CURLcode result;
+  int c;
 
-  if (request) {
-    curl_easy_setopt(request, CURLOPT_URL, "http://example.com");
-    result = curl_easy_perform(request);
-    curl_easy_cleanup(request);
+  if (request && *words) {
+    for (c = 0; *(words + c); c++) {
+      curl_easy_setopt(request, CURLOPT_URL, *uri);
+      result = curl_easy_perform(request);
+      //# Here, the script.
+      curl_easy_cleanup(request);
+    }
   }
 
   return 0;
