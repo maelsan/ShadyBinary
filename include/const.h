105bfaf26f3c68565aeb438598b3c1d2c7ad59ad
Mael <maelsan@users.noreply.github.com>
2016-02-08 16:01:44 +0100

    Username's update

--- include/const.h
+++ include/const.h
@@ -4,7 +4,7 @@
 #define VERSION puts("0.0.3");
 
 #define TITLESH puts("\033[1;36mSHADY - The vicious webscan\033[m");
-#define TITLEDE puts("-> \033[36m0.0.1\033[m | https://github.com/solikate/ShadyBinary\n");
+#define TITLEDE puts("-> \033[36m0.0.1\033[m | https://github.com/maelsan/ShadyBinary\n");
 
 #define ERRCHOS puts("\033[31m[x]\033[m Choose a option, use --help for more details.");
 #define UNKNOWO puts("\033[31m[x]\033[m Use --help for more details.");
